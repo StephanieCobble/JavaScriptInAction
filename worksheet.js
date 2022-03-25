9bb9a66c95196bfba9986bf72b5eddb1fdce5270
StephanieCobble <cobble.stephanie@gmail.com>
2022-03-25 12:37:43 -0700

    created files for js, html, and css. Everything up through for loops has been completed

---
+++ worksheet.js
@@ -0,0 +1,68 @@
+"use strict";
+
+// //Variables: 1 Days of the Week
+// let dayOfWeek = "Monday";
+// console.log(dayOfWeek);
+
+
+// dayOfWeek = "Friday"
+// console.log(`I can't wait for ${dayOfWeek}!`)
+
+// //Variables: 2 User Input
+// let animalInput = prompt(
+//     "What is your favorite animal?"
+//     );
+// let colorInput = prompt(
+//     "What is your favorite color?"
+// );
+// console.log(`I've never seen a ${colorInput} ${animalInput}!`)
+
+// //Conditionals: 1 Meals
+// //mealChoice = eggs, sandwich, crab
+// let timeOfDay = 1300
+// timeOfDay = 900
+// timeOfDay = 1800
+// timeOfDay = seven //breaks if something is given that's undefined
+// let mealChoice;
+
+// if(timeOfDay<1200) {
+//     console.log('eggs');
+// }
+// else if(timeOfDay<1700) {
+//     console.log('sandwich');
+// }
+// else {
+//     console.log('crab');
+// }
+
+// //Conditionals: 2 Random Number
+// let randomNumber = Math.floor(Math.random() * 11)
+// if(randomNumber<=2) {
+//     console.log("Beatles");
+// }
+// else if(randomNumber<=5) {
+//     console.log("Stones");
+// }
+// else if(randomNumber<=8) {
+//     console.log("Floyd");
+// }
+// else if(randomNumber<=10) {
+//     console.log("Hendrix");
+// }
+
+//Loops
+//For Loops 1-3
+// for(let i = 0; i < 7; i++) {
+//     console.log("JavaScript is cool!");
+// }
+
+// for(let i = 0; i < 11; i++) {
+//     console.log(i);
+// }
+
+// for(let i = 0; i < 5; i++) {
+//     console.log("hello");
+//     console.log("goodbye");
+// }
+
+//While Loops
